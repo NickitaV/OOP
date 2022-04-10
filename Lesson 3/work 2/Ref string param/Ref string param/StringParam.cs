e2dc33419a0beb2fc8dd0b87a3dcddfe21c049b6
Nickita V <hihollow@yandex.ru>
2022-04-10 19:56:34 +0600

    Homework_3
    Homework_3

---
+++ Lesson 3/work 2/Ref string param/Ref string param/StringParam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_string_param
+{
+    class StringParam
+    {
+        public string StringNew(ref string Hello)
+        {
+            char[] HelloNew = new char[Hello.Length];
+            int j = 0;
+            for (int i = Hello.Length - 1; i >= 0; i--)
+            {
+                HelloNew[j] = Hello[i];
+                j++;
+            }
+
+
+            string HelloReturn = new string(HelloNew);
+            return HelloReturn;
+        }
+
+    }
+}
---
+++ Lesson 3/work 3/MailList/MailList/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailList
+{
+    class Search
+    {
+
+        public string SearchMail(ref string s)
+        {
+
+            string[] newStr = s.Split(new char[] { '&' });
+            s = newStr[1];
+            return s;
+        }
+
+    }
+}
