2738567bf230b67cf6ffa502522037aeebdad8d9
Nickita V <hihollow@yandex.ru>
2022-04-21 20:00:07 +0600

    lesson 5
    lesson 5

---
+++ Lesson 5/work 1/Lesson_5_work_1/Lesson_5_work_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson_5_work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumbers rationalNumbers1 = new RationalNumbers(1,2);
+            Console.WriteLine("Первое число:"+rationalNumbers1.ToString());
+            RationalNumbers rationalNumbers2 = new RationalNumbers(3, 4);
+            Console.WriteLine("Второе число:"+rationalNumbers2.ToString());
+            RationalNumbers plus = rationalNumbers1 + rationalNumbers2;
+            Console.WriteLine(plus.ToString());
+            RationalNumbers minus = rationalNumbers1 - rationalNumbers2;
+            Console.WriteLine(minus.ToString());
+            RationalNumbers proizvedeniye = rationalNumbers1 * rationalNumbers2;
+            Console.WriteLine(proizvedeniye.ToString());
+          
+            Console.WriteLine(rationalNumbers1.Equals(rationalNumbers1));
+            Console.WriteLine(rationalNumbers1.Equals(rationalNumbers2));
+        }
+    }
+}
