5a9669ce84cf2ca2b4e912a7ad47b49799d4551c
Nickita V <hihollow@yandex.ru>
2022-04-27 21:01:39 +0600

    Homework_7
    Homework_7

---
+++ Lesson 7/Lesson8_work1/ACoder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8_work1
+{
+    class ACoder : ICoder
+    {
+        private char[] _stroke;
+
+        public ACoder(string stroke)
+        {
+            _stroke = stroke.ToCharArray();
+
+        }
+        public string Encode()
+        {
+            for (int i = 0; i < _stroke.Length; i++)
+            {
+                _stroke[i] = (char)(_stroke[i] + 1);
+            }
+            return string.Concat(_stroke);
+        }
+
+
+        public string Decode()
+        {
+            for (int i = 0; i < _stroke.Length; i++)
+            {
+                _stroke[i] = (char)(_stroke[i] - 1);
+            }
+            return string.Concat(_stroke);
+        }
+
+
+
+
+    }
+}
